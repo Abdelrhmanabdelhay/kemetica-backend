4cb899d320dfa487e1d060e331ec6e30a12d9d62
abddo <abdoabdelhay110@gmail.com>
2026-08-30 17:55:44 +0300

    feat: initialize express application with middleware and route configurations

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 app.use(cors({
-  origin: 'http://localhost:4200' || 'https://management-squeaky-fish.abasthan.app',
+  origin: ['http://localhost:4200', 'https://management-squeaky-fish.abasthan.app', 'https://kemetica.vercel.app'],
   credentials: true,
 }));
 
